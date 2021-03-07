4012e7901bb4b8c40196b09ea3d554dc1a464fac
mrzdtydlntm <mrzdtydlntm@gmail.com>
2021-03-07 17:31:42 +0700

    v0.5.5.3

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.UserProfile)
+#pass in our model to register with django admin
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+# Create a hello world APIView
+class HelloApiView(APIView):
+    #test API View
+    #HTTP GET to get a list or an object from specific API
+    def get(self, request, format=None):
+        #return a list of APIView features
+        an_apiview = [
+            'Uses HTTP methods as a function (get, post, patch, put, delete)',
+            'It is similar to a traditional django view',
+            'Give you the most control over your logic',
+            'Is mapped manually to URLs'
+        ]
+        return Response({'message':'Hello', 'an_apiview':an_apiview})
