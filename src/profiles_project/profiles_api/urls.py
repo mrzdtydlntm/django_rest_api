45311cde6c2f0b6bbca3c629a49747b081be55e6
mrzdtydlntm <mrzdtydlntm@gmail.com>
2021-03-10 22:10:46 +0700

    v0.5.5.5.2

---
+++ src/profiles_project/profiles_api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+#first parameter is name of API we want to call
+#second parameter is name of viewset that we want to assign to this router
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
+]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
 
 # Create your views here.
 
 # Create a hello world APIView
 class HelloApiView(APIView):
     #test API View
+    serializer_class = serializers.HelloSerializer
     #HTTP GET to get a list or an object from specific API
     def get(self, request, format=None):
         #return a list of APIView features
@@ -17,3 +20,36 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs'
         ]
         return Response({'message':'Hello', 'an_apiview':an_apiview})
+    
+    def post(self, request):
+        #create a hello message with our name
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk=None):
+        #Handles updating an object
+        return Response({'method':'put'})
+    
+    def patch(self, request, pk=None):
+        #Patch request, only updates fields provided in the request
+        return Response({'method':'patch'})
+    
+    def delete(self, request, pk=None):
+        #Deletes object
+        return Response({'method':'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    #test API viewset
+    def list(self, request):
+        #Return hello message
+        a_viewset=[
+            'Uses action (list, create, retreve, update, partial update)',
+            'Automatically map to URLs Routers',
+            'Provides more functionality with less code'
+        ]
+        return Response({'message':'Hello!','a_viewset':a_viewset})
